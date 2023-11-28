7e307c5967d0a02286f7d8184028b221f7be47a9
Kacper Gołuchowski <kacper.goluchowski@o2.pl>
2023-11-28 08:47:00 +0100

    inprovment

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -7,10 +7,10 @@ export default function EventsCard() {
         <section className="bg-amber-300 w-med-1 h-72 rounded-3xl font-Tsukimi p-5 md:w-med-2 md:h-48"> 
             <h1 className="text-4xl md:text-xs"> Upcoming events </h1>
             <span className="text-gray-500 ml-2"> and dates </span>
-            <ul className="mt-5">
-                <li className="text-2xl flex gap-3 my-6 md:text-xs"> <img src = {calendarPic} className="w-8 md:w-2" /> Job interviev </li>
-                <li className="text-2xl flex gap-3 my-6 md:text-xs"> <img src = {calendarPic} className="w-8 md:w-2" /> Family meeting </li>
-                <li className="text-2xl flex gap-3 my-6 md-text-xs"> <img src = {calendarPic} className="w-8 md:w-2" /> Bob's wed</li>
+            <ul className="mt-5"> 
+                <li className="text-2xl flex gap-3 my-6 md:text-xs"> <img src = {calendarPic} className="w-8 md:w-3" /> Job interviev </li>
+                <li className="text-2xl flex gap-3 my-6 md:text-xs"> <img src = {calendarPic} className="w-8 md:w-3" /> Family meeting </li>
+                <li className="text-2xl flex gap-3 my-6 md:text-xs"> <img src = {calendarPic} className="w-8 md:w-3" /> Bob's weddding </li>
             </ul>
         </section>
     )
--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -5,12 +5,12 @@ import menuPic from '../images/menu-pic.png';
 
 export default function Logo() {
     return (
-        <section className="flex justify-between md:w-11/12">
+        <section className="flex justify-between items-center md:w-full">
             <div className="flex items-center justify-center text-med-1 w-96 h-32 gap-4 font-Ubuntu md:text-3xl md:h-16 md:gap-2 md:w-64 md:my-2">
                 <img src = {logoPic} className="w-16 md:w-12"/>
                 <h1> LifeTracker </h1>
             </div>
-            <img src = {menuPic} className="h-16 desktop:hidden md:hidden" />
+            <img src = {menuPic} className="h-16 desktop:hidden" />
         </section>
     )
 }
