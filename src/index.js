14a1d0c8e496f3a42e5d19c10098c184c500a7f6
Kacper Gołuchowski <kacper.goluchowski@o2.pl>
2023-11-28 09:52:55 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const router = createBrowserRouter([
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <div className='h-min w-screen bg-gradient-to-br from-amber-200 to-amber-300 flex justify-center items-center'>
+    <div className='h-min w-screen bg-gradient-to-br from-amber-200 to-amber-300 flex justify-center items-center py-4'>
       <RouterProvider router={router} />
     </div>
   </React.StrictMode>
