c7335696e32eb3bd334f14b8bcadd19caaf604c4
Kacper Gołuchowski <kacper.goluchowski@o2.pl>
2023-11-29 20:03:00 +0100

    add responsive to login page

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,18 +6,18 @@ import applePic from '../images/apple-pic.png';
 
 export default function Login() {
     return (
-        <section className="bg-gradient-to-br from-amber-200 to-amber-300 w-1/3 h-4/6 py-5 px-5 rounded-3xl"> 
-            <h1 className="text-4xl mb-4 font-Ubuntu"> Login </h1>
-            <span className="text-2xl font-Tsukimi"> Welcome onboard with us! </span>
+        <section className="bg-gradient-to-br from-amber-200 to-amber-300 w-1/3 h-4/6 py-5 px-5 rounded-3xl md:w-full"> 
+            <h1 className="text-4xl mb-4 font-Ubuntu md:text-center"> Login </h1>
+            <span className="text-2xl font-Tsukimi md:hidden"> Welcome onboard with us! </span>
             <div className="mt-10">
                 <div className="flex flex-col">
                     <span className="text-2xl ml-4 font-Tsukimi"> Username </span>
-                    <input type = 'text' placeholder="Enter your username" className="w-5/6 h-12 rounded-3xl px-5 font-Ubuntu"/>
+                    <input type = 'text' placeholder="Enter your username" className="w-5/6 h-12 rounded-3xl px-5 font-Tsukimi"/>
                 </div>
                 <div className="flex flex-col mt-5">
                     <span className="text-2xl ml-4 font-Tsukimi"> Password </span>
-                    <input type = 'password' placeholder="Enter your password" className="w-5/6 h-12 rounded-3xl px-5 font-Ubuntu"/>
-                    <span className="text-xl ml-auto mr-24 text-slate-500 cursor-pointer hover:text-slate-800 font-Tsukimi"> Forgot password? </span>
+                    <input type = 'password' placeholder="Enter your password" className="w-5/6 h-12 rounded-3xl px-5 font-Tsukimi"/>
+                    <span className="text-xl ml-auto mr-24 text-slate-500 cursor-pointer hover:text-slate-800 font-Tsukimi md:mr-14"> Forgot password? </span>
                 </div>
                 <div className="flex gap-6 mt-5 h-16">
                     <LoginCard image = {fbPic}/>
@@ -25,6 +25,7 @@ export default function Login() {
                     <LoginCard image = {applePic}/>
                 </div>
                 <button className="bg-amber-400 w-5/6 h-12 mt-5 text-2xl rounded-3xl font-Ubuntu hover:bg-amber-500"> Login </button>
+                <span className="block font-Tsukimi ml-5 mt-5 cursor-pointer"> New to LifeTracker? <p className="inline text-white"> Register Here </p></span>
             </div>
         </section>
     )
--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -9,7 +9,7 @@ export default function LoginPage() {
        <div className="bg-amber-100 shadow-lg shadow-gray-800/100 w-11/12 h-wrapper-height rounded-3xl px-5 py-3 md:px-2 md:mt-0 md:h-min">
             <Logo />
             <div className="flex items-center justify-around">
-                <img src = {loginPic} className="w-med-3" />
+                <img src = {loginPic} className="w-med-3 md:hidden" />
                 <Login />
             </div>
         </div>
