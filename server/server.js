b55084bade7cf17e9fc23d9ef0c0ed48814ac850
Kacper Gołuchowski <kacper.goluchowski@o2.pl>
2023-12-01 12:30:04 +0100

    add axios and server

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 9000; // Używaj zmiennej 'port' zamiast 'process.env.PORT'
 const MONGO_URI = process.env.MONGO_URI;
 const UserModel = require("./models/User");
 const cors = require('cors');
@@ -13,26 +13,36 @@ const options = {
 };
 
 app.use(cors());
+app.use(express.json()); // Dodaj middleware do obsługi danych JSON w żądaniach
 
-mongoose
-  .connect(MONGO_URI, options)
+// Połącz z bazą danych na starcie serwera
+mongoose.connect(MONGO_URI, options)
   .then(() => {
     console.log("Pomyślnie połączono z bazą danych");
-    
-    app.listen(port, () => {
-      console.log("Serwer uruchomiony na porcie ", process.env.PORT);
-      const newUser = new UserModel({
-        email: "dyziu@o2.pl",
-        nickname: "marekgoluchowski",
-        password: "renatka1",
-      });
 
-      newUser
-        .save()
-        .then((result) => console.log("Dodano użytkownika", result))
-        .catch((err) => console.log("BŁAD: ", err));
+    // Uruchom serwer po połączeniu z bazą danych
+    app.listen(port, () => {
+      console.log("Serwer uruchomiony na porcie ", port);
     });
   })
   .catch((err) => {
     console.error("Błąd połączenia z bazą danych:", err);
   });
+
+// Obsługa żądania POST /addUser
+app.post('/addUser', async (req, res) => {
+  try {
+    const newUser = new UserModel({
+      email: "dyziu@o2.pl",
+      nickname: "marekgoluchowski",
+      password: "renatka1",
+    });
+
+    const savedUser = await newUser.save();
+    console.log("Dodano użytkownika", savedUser);
+    res.status(201).json({ message: 'Użytkownik dodany pomyślnie', user: savedUser });
+  } catch (error) {
+    console.log("BŁAD: ", error);
+    res.status(500).json({ error: 'Błąd podczas dodawania użytkownika' });
+  }
+});
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
+import axios from "axios";
 import '../index.css';
 import fbPic from '../images/fb-pic.png';
 import googlePic from '../images/google-pic.png';
 import applePic from '../images/apple-pic.png';
 
 export default function Login() {
+    const nicknameRef = useRef(0);
+    const passwordRef = useRef(0);
+
+    function loginUser() {
+        const postData = {
+            nickname: nicknameRef.current.value,
+            password: passwordRef.current.value
+        }
+        axios.post('http://localhost:9000/addUser', postData);
+    }
+
     return (
         <section className="bg-gradient-to-br from-amber-200 to-amber-300 w-1/3 h-4/6 py-5 px-5 rounded-3xl md:w-full md:h-screen"> 
             <h1 className="text-4xl mb-4 font-Ubuntu md:text-center"> SIGN IN </h1>
@@ -12,11 +24,11 @@ export default function Login() {
             <div className="mt-10">
                 <div className="flex flex-col">
                     <span className="text-2xl ml-4 font-Tsukimi"> Username </span>
-                    <input type = 'text' placeholder="Enter your username" className="w-5/6 h-12 rounded-3xl px-5 font-Tsukimi md:w-full"/>
+                    <input type = 'text' placeholder="Enter your username" className="w-5/6 h-12 rounded-3xl px-5 font-Tsukimi md:w-full" ref={nicknameRef}/>
                 </div>
                 <div className="flex flex-col mt-5">
                     <span className="text-2xl ml-4 font-Tsukimi"> Password </span>
-                    <input type = 'password' placeholder="Enter your password" className="w-5/6 h-12 rounded-3xl px-5 font-Tsukimi md:w-full"/>
+                    <input type = 'password' placeholder="Enter your password" className="w-5/6 h-12 rounded-3xl px-5 font-Tsukimi md:w-full" ref={passwordRef}/>
                     <span className="text-xl ml-auto mr-24 text-slate-500 cursor-pointer hover:text-slate-800 font-Tsukimi md:mr-0"> Forgot password? </span>
                 </div>
                 <div className="flex gap-6 mt-5 h-16 md:justify-around">
@@ -24,7 +36,7 @@ export default function Login() {
                     <LoginCard image = {googlePic}/>
                     <LoginCard image = {applePic}/>
                 </div>
-                <button className="bg-amber-400 w-5/6 h-12 mt-5 text-2xl rounded-3xl font-Ubuntu hover:bg-amber-500 md:w-full"> Login </button>
+                <button className="bg-amber-400 w-5/6 h-12 mt-5 text-2xl rounded-3xl font-Ubuntu hover:bg-amber-500 md:w-full" onClick={loginUser}> Login </button>
                 <span className="block font-Tsukimi ml-5 mt-5"> New to LifeTracker? <p className="inline text-white cursor-pointer"> Register Here </p></span>
             </div>
         </section>
